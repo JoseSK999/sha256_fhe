3f2786063bd9d457cf3836267c94dbf93f142e17
Sexosexosexo <83597627+JoseSK999@users.noreply.github.com>
2023-05-03 19:19:20 +0200

    hash stdin

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,35 @@ mod padding;
 mod boolean_ops;
 mod sha256;
 
+use std::io;
 use tfhe::boolean::prelude::*;
 use padding::pad_sha256_input;
 use sha256::{sha256_fhe, bools_to_hex};
 
 fn main() {
-    let (ck, sk) = gen_keys();
+    // INTRODUCE INPUT FROM STDIN
+
+    let mut input = String::new();
+    println!("Write input to hash:");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input = input.trim_end_matches('\n').to_string();
+
+    println!("You entered: \"{}\"", input);
 
     // CLIENT PADS DATA AND ENCRYPTS IT
 
-    let padded_input = pad_sha256_input("hello world");
+    let (ck, sk) = gen_keys();
+
+    let padded_input = pad_sha256_input(&input);
     let encrypted_input = encrypt_bools(&padded_input, &ck);
 
     // SERVER COMPUTES OVER THE ENCRYPTED PADDED DATA
 
+    println!("Computing the hash");
     let encrypted_output = sha256_fhe(encrypted_input, &sk);
 
     // CLIENT DECRYPTS THE OUTPUT
